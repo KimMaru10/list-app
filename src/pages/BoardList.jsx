09bb461f93b04a0b2578f1d53c00a15641fa68c8
김대성 <daesung3725@naver.com>
2024-05-02 00:45:52 +0900

    fix : showList 추가하여 상태 관리

--- src/pages/BoardList.jsx
+++ src/pages/BoardList.jsx
@@ -4,11 +4,12 @@ import { getBoardList } from '../apis/axios.js';
 const BoardList = () => {
   const navigate = useNavigate();
   const [boardList, setBoardList] = useState([]);
-
+  const [showlist, setShowList] = useState(false);
   const fetchBoardList = async () => {
     try {
       const resp = await getBoardList(); // axios 모듈에서 데이터 가져오기
       setBoardList(resp);
+      setShowList(true);
     } catch (error) {
       console.error("리스트 불러오기 실패",error);
     }
@@ -23,13 +24,17 @@ const BoardList = () => {
 
   return (
     <div> 
-        <ul>
-          {boardList && boardList.map((board) => ( // 4) map 함수로 데이터 출력
-            <li key={board.id}>
-              <Link to={`/board/${board.id}`}>{board.title}</Link>
-            </li>
-          ))}
-        </ul>
+      {showlist ? (
+          <ul>
+            {boardList && boardList.map((board) => (
+              <li key={board.id}>
+                <Link to={`/board/${board.id}`}>{board.title}</Link>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>게시물이 렌더링 되지 않았습니다.</p>
+        )}
       <div>
         <button onClick={moveToWrite}>글쓰기</button>
       </div>
