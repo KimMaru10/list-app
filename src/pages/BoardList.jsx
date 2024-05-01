7e862aa7492cee78e1e02bf46e4d1c4a27dac079
김대성 <daesung3725@naver.com>
2024-05-01 23:24:03 +0900

    fix : renderBoardList를 만들어서 fetch된 이후에 수행하도록 함

--- src/pages/BoardList.jsx
+++ src/pages/BoardList.jsx
@@ -4,37 +4,42 @@ import { getBoardList } from '../apis/axios.js';
 const BoardList = () => {
   const navigate = useNavigate();
   const [boardList, setBoardList] = useState([]);
-  
+
+  const fetchBoardList = async () => {
+    try {
+      const resp = await getBoardList(); // axios 모듈에서 데이터 가져오기
+      setBoardList(resp);
+    } catch (error) {
+      console.error("리스트 불러오기 실패",error);
+    }
+  };
+
   useEffect(() => {
-    const fetchBoardList = async () => {
-      try {
-        const resp = await getBoardList(); // axios 모듈에서 데이터 가져오기
-        setBoardList(resp);
-      } catch (error) {
-        console.error("리스트 불러오기 실패",error);
-      }
-    };
-    fetchBoardList();
+    fetchBoardList(); // 1) 게시글 목록 조회 함수 호출
   }, []);
-  
   
   const moveToWrite = () => {
     navigate('/write');
   };
-  
 
+  const renderBoardList = () => {
+    if (boardList && boardList.length > 0) {
+      return (
+        <ul>
+          {boardList.map((board) => (
+            <li key={board.id}>
+              <Link to={`/board/${board.id}`}>{board.title}</Link>
+            </li>
+          ))}
+        </ul>
+      );
+    } else {
+      return <p>게시글 목록이 비어 있습니다.</p>;
+    }
+  };
   return (
     <div>
-        {boardList && boardList.length > 0 && (
-          <ul>
-            {boardList.map((board) => (
-              // 4) map 함수로 데이터 출력
-              <li key={board.id}>
-                <Link to={`/board/${board.id}`}>{board.title}</Link>
-              </li>
-            ))}
-          </ul>
-        )}
+       {renderBoardList()}
       <div>
         <button onClick={moveToWrite}>글쓰기</button>
       </div>
