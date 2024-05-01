b04e4f6dd847d57955a3284e1d501ef5507d42c9
김대성 <daesung3725@naver.com>
2024-05-02 00:03:42 +0900

    fix : ? 추가

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export {default as Header} from './layout/Header';
 export {default as Footer} from './layout/Footer';
 export {default as Images} from './Images';
 export {default as Board} from './Board';
-export {default as MapBoardList} from './MapBoardList';
--- src/pages/BoardList.jsx
+++ src/pages/BoardList.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { getBoardList } from '../apis/axios.js';
-import { MapBoardList } from '../components/index.js';
 const BoardList = () => {
   const navigate = useNavigate();
   const [boardList, setBoardList] = useState([]);
@@ -14,7 +13,6 @@ const BoardList = () => {
       console.error("리스트 불러오기 실패",error);
     }
   };
-
   useEffect(() => {
     fetchBoardList(); // 1) 게시글 목록 조회 함수 호출
   }, []);
@@ -22,9 +20,18 @@ const BoardList = () => {
   const moveToWrite = () => {
     navigate('/write');
   };
+
   return (
     <div>
-       <MapBoardList boardList={boardList}/>
+        <ul>
+          {boardList?.map((board) => (
+              // 4) map 함수로 데이터 출력
+            <li key={board.id}>
+              <Link to={`/board/${board.id}`}>{board.title}</Link>
+            </li>
+          ))
+          }
+        </ul>
       <div>
         <button onClick={moveToWrite}>글쓰기</button>
       </div>
